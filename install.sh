619614d976791525b1de804f91836289afb95f7e
Chandan <chandank@live.ca>
2020-05-30 12:53:04 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 yum install https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
-yum install postgresql12 -y
-yum install postgresql12-server -y
-usr/pgsql-12/bin/postgresql-12-setup initdb
+yum install postgresql12 postgresql12-contrib postgresql12-server -y
 /usr/pgsql-12/bin/postgresql-12-setup initdb
 systemctl enable postgresql-12; systemctl start postgresql-12
 su - postgres
